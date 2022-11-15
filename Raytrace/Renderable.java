b72c16c1d1888567be90fe21c4b1e869a72b6ab3
Rheena Blas <73996643+rheenablas@users.noreply.github.com>
2022-11-15 22:42:24 +0000

    Created javadocs comment in sphere.java

--- Raytrace/Renderable.java
+++ Raytrace/Renderable.java
@@ -3,6 +3,9 @@ package Raytrace;
 import java.awt.*;
 import java.util.List;
 
+/**
+ * Renderable is the interface
+ */
 public interface Renderable {
     boolean intersect(Ray r);
     Color Shade(Ray ray, java.util.List<Object> lights, List<Object> objects, Color background);
